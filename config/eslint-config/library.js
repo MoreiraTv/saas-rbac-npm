fb3a04e31894985d11290eaeef112b123a675a4c
Matheus Moreira <matheusmoreira08@gmail.com>
2024-07-08 21:22:29 -0300

    Correção dos pacotes de config; Criado o pacote de auth com papeis e permissoes;

--- config/eslint-config/library.js
+++ config/eslint-config/library.js
@@ -1,8 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@rocketseat/eslint-config/react"],
-  plugins: ["Simple-import-sort"],
+  extends: ['@rocketseat/eslint-config/react'],
+  plugins: ['simple-import-sort'],
   rules: {
-    "simple-import-sort/imports": "error"
-  }
-};
+    'simple-import-sort/imports': 'error',
+  },
+}
